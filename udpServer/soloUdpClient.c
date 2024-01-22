203cc2b91d531ebb9f8c8039dd9af1926c24af96
choclollipop <2924733894@qq.com>
2024-01-22 19:41:58 +0800

    [新增] 新增UDP中JSON代码

--- udpServer/soloUdpClient.c
+++ udpServer/soloUdpClient.c
@@ -9,6 +9,8 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <signal.h>
+#include <json-c/json.h>
+#include <json-c/json_object.h>
 
 #define PORT    6666
 #define ADDR    "172.30.149.120"
@@ -17,6 +19,9 @@
 
 int main()
 {
+    /* 新建json对象 */
+    struct json_object * object = json_object_new_object();
+
     /* 套接字 */
     int socketFd = socket(AF_INET, SOCK_DGRAM, 0);
     if (socketFd < 0)
@@ -36,15 +41,24 @@ int main()
     socklen_t recvAddrLen = sizeof(recvAddrLen);
 
 
-    char buffer[BUFFER_SIZE];
-    memset(buffer, 0, sizeof(buffer));
+    // char buffer[BUFFER_SIZE];
+    // memset(buffer, 0, sizeof(buffer));
 
     char recvBuffer[BUFFER_SIZE];
     memset(recvBuffer, 0, sizeof(recvBuffer));
 
-    strncpy(buffer, "abcdefACD", sizeof(buffer) - 1);
+    /* 1.注册 2.登录 */
+    /* {"way" : 1} */
+    struct json_object * wayVal = json_object_new_int64(1);
+    json_object_object_add(object, "way", wayVal);
 
-    sendto(socketFd, buffer, sizeof(buffer), 0, (struct sockaddr *)&serverAddr, serverAddrLen);
+    /* 将json对象转换成字符串 */
+    const char * ptr = json_object_to_json_string(object);
+
+    // strncpy(buffer, "abcdefACD", sizeof(buffer) - 1);
+
+    /* 将对象发送给服务器 */
+    sendto(socketFd, ptr, strlen(ptr), 0, (struct sockaddr *)&serverAddr, serverAddrLen);
 
     recvfrom(socketFd, recvBuffer, sizeof(recvBuffer), 0, (struct sockaddr *)&recvAddr, &recvAddrLen);
 
@@ -52,6 +66,8 @@ int main()
 
     close(socketFd);
     
+    /* 释放object对象 */
+    json_object_put(object);
     
 
     return 0;
