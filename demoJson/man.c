95d79c3382598c0cff009a5ba2088c883534786b
choclollipop <2924733894@qq.com>
2024-01-24 09:29:08 +0800

    [新增] 新增json示例代码

--- demoJson/man.c
+++ demoJson/man.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
 #include <json-c/json.h>
 #include <json-c/json_object.h>
+#include <unistd.h>
 
 int main()
 {
     /* 新进json对象 */
     struct json_object * jsonObj = json_object_new_object();
+    if (jsonObj == NULL)
+    {
+        perror("new json object error");
+        exit(-1);
+    }
 
-    struct json_object * value1 = json_object_new_int64(32);
+    struct json_object * wayValue = json_object_new_int64(32);
+    if (wayValue == NULL)
+    {
+        perror("new json object error");
+        exit(-1);
+    }
 
-    json_object_object_add(jsonObj, "age", value1);
+    int ret = json_object_object_add(jsonObj, "way", wayValue);
+    if (ret < 0)
+    {
+        /* 释放内存 .....*/
+
+    }
+
+    struct json_object * nameValue = json_object_new_string("zhangsan");
+
+    ret = json_object_object_add(jsonObj, "name", nameValue);
+    if (ret < 0)
+    {
+
+    }
 
     const char * ptr = json_object_to_json_string(jsonObj);
 
